cb19b85875781b665c65b4b98caea3e976f4c1fc
zhangtingyuvery <zhangtingyu@weiresearch.com>
2020-06-15 08:58:58 +0800

    修复一些问题

--- app/templates/project/src/main.js
+++ app/templates/project/src/main.js
@@ -1,13 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import MintUI from 'mint-ui'
-import 'mint-ui/lib/style.css'
 import '@assets/normalize.css'
 
 import store from '@/store'
-
-Vue.use(MintUI)
 
 Vue.config.productionTip = false
 
