e6406a7ed4f25761d8f8a9a3b12d681ed7e36820
zhangtingyuvery <zhangtingyu@weiresearch.com>
2022-04-14 09:52:08 +0800

    配置项

--- app/templates/project/vue.config.js
+++ app/templates/project/vue.config.js
@@ -1,25 +1,38 @@
-const path = require('path');
+const path = require('path')
 
 function resolve(dir) {
 	return path.join(__dirname, dir);
 }
 
 module.exports = {
+	publicPath: './',
+	lintOnSave: process.env.NODE_ENV !== 'production',
+	productionSourceMap: false,
+	chainWebpack: config => {
+		config.resolve.alias
+			.set('@', resolve('src'))
+			.set('@assets', resolve('src/assets'))
+			.set('@common', resolve('src/common'))
+	},
 	css: {
 		loaderOptions: {
 			sass: {
-				data: `
-					@import "@/common/scss/mixin.scss";
-					@import "@/common/scss/variable.scss";
-				`
+				prependData: `@import "@/common/scss/mixin.scss";
+							  @import "@/common/scss/variable.scss";`
 			}
 		}
 	},
-	publicPath: './',
-	chainWebpack: (config) => {
-			config.resolve.alias
-				.set('@', resolve('src'))
-				.set('@assets', resolve('src/assets'))
-				.set('@common', resolve('src/common'))
+	devServer: {
+		host: "localhost",
+		port: 3000,
+		proxy: {
+			"/api": {
+				target: "https://geoapi.qweather.com",
+				changeOrigin: true,
+				pathRewrite: {
+					'^/api': '/'
+				}
+			}
 		}
+	}
 }
